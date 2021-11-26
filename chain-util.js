62ca32dfba72ef27bcbf46fc941e45a0cdf520f9
Rogerio de Leon Pereira <vinashu@gmail.com>
2021-11-26 16:48:29 -0600

    Test transactions and unique ID

--- chain-util.js
+++ chain-util.js
@@ -1,9 +1,14 @@
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
+const { v1: uuidV1 } = require('uuid');
 
 class ChainUtil {
     static genKeyPair() {
         return ec.genKeyPair();
+    }
+
+    static id() {
+        return uuidV1();
     }
 }
 
