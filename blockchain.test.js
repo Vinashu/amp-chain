7b075fb92c5b549a938cd55570ae498dcf4242f1
Rogerio de Leon Pereira <vinashu@gmail.com>
2021-11-25 22:30:42 -0600

    Added the blockchain validation and unity test

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,11 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('Blockchain', () => {
-    let bc;
+    let bc, bc2;
 
     beforeEach(() => {
         bc = new Blockchain();
+        bc2 = new Blockchain();
     });
 
     it('starts with genesis block', () => {
@@ -19,4 +20,27 @@ describe('Blockchain', () => {
         expect(bc.chain[bc.chain.length-1].data).toEqual(data);
     })
 
+    it('validates a valid chain', () => {
+        const data = 'test string';        
+        bc2.addBlock(data);
+
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+    });
+
+    it('invalidates a chain with a corrupt genesis block', () => {
+        const data = 'corrupted test string';
+        bc2.chain[0].data = data;
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('invalidates a corrupt chain', () => {
+        const data = 'test string';        
+        const dataCorrupted = 'corrupted test string';
+
+        bc2.addBlock(data);
+        bc2.chain[1].data = dataCorrupted;
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });    
 });
