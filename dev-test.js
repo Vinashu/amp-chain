25a9d6423c848da1ab6533e64a3afb413e6b57cc
Rogerio de Leon Pereira <vinashu@gmail.com>
2021-11-26 16:17:26 -0600

    Added wallet and keypair

--- dev-test.js
+++ dev-test.js
@@ -1,26 +1,3 @@
-const Blockchain = require('./blockchain');
-
-const bc = new Blockchain();
-
-/*
-const block = new Block(
-    'timestamp test',
-    'last hash test',
-    'hash test',
-    'data test'
-);
-
-console.log(block.toString());
-console.log(Block.genesis().toString());
-
-const testBlock = Block.mineBlock(
-    Block.genesis(),
-    'test string'
-);
-
-console.log(testBlock.toString());
-*/
-
-for(let i=0; i<10; i++){
-    console.log(bc.addBlock(`test ${i}`).toString());
-}
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.toString());
--- wallet/index.js
+++ wallet/index.js
@@ -1,15 +1,16 @@
+const ChainUtil = require('../chain.util');
 const { INITIAL_BALANCE } = require('../config');
 
 class Wallet {
     constructor() {
         this.balance = INITIAL_BALANCE;
-        this.keyPar = null;
-        this.publicKey = null;
+        this.keyPar = ChainUtil.genKeyPair();
+        this.publicKey = this.keyPar.getPublic().encode('hex');
     }
 
     toString() {
         return `Wallet -
-            publicKey: ${this.publicKey.toString()}
+            publicKey: ${this.publicKey.toString().substring(0,16)}...${this.publicKey.toString().substring(this.publicKey.toString().length-4)}
             balance  : ${this.balance}`;
     }
 }
