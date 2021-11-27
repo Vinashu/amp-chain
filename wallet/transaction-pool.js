fed6b66eb471851047ba1b9922344af067ad0b55
Rogerio de Leon Pereira <vinashu@gmail.com>
2021-11-26 20:15:50 -0600

    Wallect transactionsa and tests

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -5,12 +5,16 @@ class TransactionPool {
 
     updateOrAddTransaction(transaction) {
         let transactionWithId = this.transactions.find(t => t.id === transaction.id);
-        
+
         if(transactionWithId) {
             this.transactions[this.transactions.indexOf(transactionWithId)] = transaction;
         } else {
             this.transactions.push(transaction);
         }
+    }
+
+    existingTransaction(address) {
+        return this.transactions.find(t => t.input.address === address);
     }
 }
 
