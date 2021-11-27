c5ea20fcc69bedd70c9750cc5238cead5f81c95e
Rogerio de Leon Pereira <vinashu@gmail.com>
2021-11-26 21:24:59 -0600

    Test of validating transactions

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -8,8 +8,9 @@ describe('TransactionPool', () => {
     beforeEach(() => {
         tp = new TransactinPool();
         wallet = new Wallet();
-        transaction = Transaction.newTransaction(wallet, 'Recipient', 30);
-        tp.updateOrAddTransaction(transaction);
+        // transaction = Transaction.newTransaction(wallet, 'Recipient', 30);
+        // tp.updateOrAddTransaction(transaction);
+        transaction = wallet.createTransaction('Recipient', 30, tp);
     });
 
     it('add a transaction to the pool', () => {
@@ -22,5 +23,30 @@ describe('TransactionPool', () => {
         tp.updateOrAddTransaction(newTransaction);
         expect(tp.transactions.find(t => t.id === newTransaction.id))
         .not.toEqual(oldTransaction);
+    });
+
+    describe('mixing valid and corupt transactions', () => {
+        let validTransactions;
+
+        beforeEach(() => {
+            validTransactions = [... tp.transactions];
+            for(let i=0; i<6; i++) {
+                wallet = new Wallet();
+                transaction = wallet.createTransaction('Recipient Address', 30, tp);
+                if(i % 2 == 0) {
+                    transaction.input.amount = 99999;
+                } else {
+                    validTransactions.push(transaction);
+                }
+            }
+        });
+
+        it('shows a diffence between valid and corrupt transactions', () => {
+            expect(JSON.stringify(tp.transaction)).not.toEqual(JSON.stringify(validTransactions));
+        });
+
+        it('grabs valid transactions', () => {
+            expect(tp.validTransactions()).toEqual(validTransactions);
+        });
     });
 })
